7d8c2dfb5ff2292a29141836eae5812aced9c0c5
Vitor Cruz <vitorcolatusso@gmail.com>
2026-03-17 21:27:40 -0300

    aula 2

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "pessoa.h"
 
 int main(){
-    Pessoa Vitor(27, 2, 2005);
+    Pessoa Vitor(10, 3, 2009);
     Vitor.calc_idade(10, 3, 2026);
     cout << Vitor.inform_nome() << " tem " << Vitor.inform_idade() << " anos" << endl;
     getchar();
