34e64bab2248f26ff141d481aa2ec86cf737d187
Vitor Cruz <vitorcolatusso@gmail.com>
2026-03-30 22:00:45 -0300

    classes lista

--- elaluno.cpp
+++ elaluno.cpp
@@ -4,6 +4,7 @@
 ElAluno::ElAluno(){
     pAluno = NULL;
     nextElAluno = prevElAluno = NULL;
+    p1 = p2 = final = faltas = 0;
 }
 
 ElAluno::~ElAluno(){
@@ -22,3 +23,20 @@ Aluno* ElAluno::getAluno(){
 const char* ElAluno::getNome(){
     return pAluno->inform_nome();
 }
+
+void ElAluno::ins_p1(int nota_p1){
+    p1 = nota_p1;
+}
+
+void ElAluno::ins_p2(int nota_p2){
+    p2 = nota_p2;
+}
+
+void ElAluno::ins_final(int nota_final){
+    final = nota_final;
+}
+
+void ElAluno::ins_falta(int falta){
+    faltas+=falta;
+}
+
--- elaluno.h
+++ elaluno.h
@@ -4,12 +4,17 @@
 class ElAluno{
 private:
     Aluno* pAluno;
+    int p1, p2, final, faltas;
 public:
     ElAluno();
     ~ElAluno();
     void setAluno(Aluno *pa);
     Aluno* getAluno();
     const char* getNome();
+    void ins_p1(int nota_p1);
+    void ins_p2(int nota_p2);
+    void ins_final(int nota_final);
+    void ins_falta(int falta = 1);
     ElAluno *nextElAluno, *prevElAluno;
 
 };
